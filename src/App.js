bea97a26731deb21337d152548a8f9db477b4279
ngecu <devngecu@gmail.com>
2020-12-05 21:28:14 +0300

    conversion all good.Next is image of flag

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from "axios";
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    state = {
+      number : " ",
+        rates : [],
+        fromCurrency : "",
+        toCurrency : "",
+        USD : " ",
+        convertedAmount : " ",
+        country : []
+
+
+    }
+    componentDidMount(){
+      this.fetchExchangeRate();
+    }
+
+  
+
+     getCountries = (toCurrency) => {
+      try {
+          const response =  axios.get(`https://restcountries.eu/rest/v2/currency/${toCurrency}`);
+          
+      // return response.data.map(country => country.name);
+      } catch (error) {
+          throw new Error(`Unable to get countries that use ${toCurrency}`);
+      }
+      
+  }
+
+    
+    fetchExchangeRate = () => {
+        axios.get('http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1')
+        .then((response)=>{
+            const rates = response.data.rates;
+            this.setState({rates})
+        })
+    }
+
+
+
+
+    change = (e) => {
+      const number = e.target.value
+      this.setState({number})
+
+    }
+
+    fromCurrencyFunc = (e) => {
+      const fromCurrency = e.target.value
+      this.setState({fromCurrency})
+    }
+
+
+    toCurrencyFunc = (e) => {
+      const toCurrency = e.target.value
+      this.setState({toCurrency})
+      this.getCountries(toCurrency)
+
+    }
+
+    Convert = () => {
+      const rate = this.state.rates
+      const USD = 1/rate[this.state.fromCurrency]
+      const exchangeRate = USD * rate[this.state.toCurrency]
+      const convertedAmount = (this.state.number * exchangeRate).toFixed(2)
+      this.setState({convertedAmount})
+      
+    }
+
+
+    render() {
+      const object = this.state.rates
+        return (
+          <div>
+
+            <input onChange={this.change} type="Number">
+            
+            </input>
+            From:
+            <select onChange={this.fromCurrencyFunc} >
+              {Object.entries(object).map(([key,value],i) => 
+    <option selected={i === 0} key={i} value={key} >{key}</option>
+)
 }
+            </select>
 
-export default App;
+To:
+           
+            <select onChange={this.toCurrencyFunc}>
+              {Object.entries(object).map(([key,value],i) => 
+    <option key={i} value={key} >{key}</option>
+)
+}
+            </select>
+            <p> from currency : {this.state.fromCurrency}
+            </p>
+            <p> to currency : {this.state.toCurrency}
+            </p>
+            <p>
+              Amount : {this.state.number}
+            </p>
+            <p>
+             Converted Ammount : {this.state.convertedAmount}
+            </p>
+            <button onClick={this.Convert}>
+              Convert
+            </button>
+       
+
+      
+   
+
+            
+          </div>
+        )
+    }
+}
